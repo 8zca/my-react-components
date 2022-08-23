0b14dd10dca77ae35a7f32d5c34f6ea2876bf645
8zca <excref@gmail.com>
2022-08-23 11:51:17 +0900

    [Add] button sample

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const Button = styled.button`
+  box-sizing: border-box;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  appearance: none;
+`
