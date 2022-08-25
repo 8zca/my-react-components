1006dad30b803c99e7b207cb26df836dc29365b6
8zca <excref@gmail.com>
2022-08-25 10:38:36 +0900

    [Update] relative path

--- src/components/Button/Button2.tsx
+++ src/components/Button/Button2.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled'
-import { Button } from '@/components/Button/Button'
+import { Button } from './Button'
 
 export const Button2 = styled(Button)`
   background: #ccc;
