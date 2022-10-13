dabba38da14f35609c2e5d98cea6fd941fe980c8
8zca <excref@gmail.com>
2022-10-13 22:47:03 +0900

    fix: update

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,31 @@
-import { babel } from '@rollup/plugin-babel'
-import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
-import { terser } from 'rollup-plugin-terser'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
 const packageJson = require('./package.json')
 
 export default [
-    {
-        input: 'src/index.ts',
-        output: [
-            {
-                file: packageJson.main,
-                format: 'cjs',
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: 'esm',
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            peerDepsExternal(),
-            resolve(),
-            commonjs(),
-            // eslint-disable-next-line no-undef
-            typescript({ tsconfig: './tsconfig.json' }),
-            terser(),
-            babel({
-                babelHelpers: 'bundled',
-            })
-        ],
-        external: ['react', 'react-dom', '@emotion/react', '@emotion/styled']
-    },
-    {
-        input: 'dist/esm/index.d.ts',
-        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-        plugins: [dts()],
-    },
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    plugins: [peerDepsExternal(), resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })]
+  },
+  {
+    input: 'dist/esm/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()]
+  }
 ]
--- src/components/Button/Button3.tsx
+++ src/components/Button/Button3.tsx
@@ -1,9 +1,14 @@
+import { css } from '@emotion/react'
 import React, { ComponentPropsWithoutRef } from 'react'
 
 export type ButtonProps = {
   text: string
 } & ComponentPropsWithoutRef<'button'>
 
+const style = css({
+  background: '000'  
+})
+
 export const Button3: React.FC<ButtonProps> = ({ text, ...props }) => {
-  return <button {...props} css={{ background: '#000' }}>{text}</button>
+  return <button {...props} css={style}>{text}</button>
 }
